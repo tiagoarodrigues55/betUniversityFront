e91a13319d8882eef02bb3fc2d52d27c8a02ad2f
Gabriel9Stresser <gabriel.augusto99@hotmail.com>
2022-05-21 17:52:10 -0300

    refact home

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,14 +1,32 @@
 import { useState, useEffect } from 'react';
-
-import Router from 'next/router';
+import Image from 'next/image';
 
 import axios from 'axios';
 
 import styles from '../../styles/Home.module.css';
 
-import { supabase } from '../../services/supabaseClient';
+import { Event } from '../../Types';
+import { useAuth } from '../../hooks/auth/auth';
 
-import { User, Event } from '../../Types';
+import { Navbar } from '../../components/Navbar';
+import {
+	CardDiv,
+	CardsSection,
+	ContainerReturnStyled,
+	ContainerStyled,
+	ContainerTemplateStyled,
+	TwoCards,
+	TwoCardsSecond,
+} from './styles';
+
+import GoogleIcon from '../../assets/googleIcon.png';
+import Link from 'next/link';
+import { Card } from '../../components/ModalityCard';
+
+import { FaFutbol } from 'react-icons/fa';
+import { MdSportsHandball } from 'react-icons/md';
+import { GiPartyPopper } from 'react-icons/gi';
+import { BsCalendarEvent } from 'react-icons/bs';
 
 interface Card {
 	name: string;
@@ -16,7 +34,8 @@ interface Card {
 }
 
 export default function Login() {
-	const [user, setUsers] = useState<User>();
+	const { setData, user } = useAuth();
+
 	const [logged, setLogged] = useState(false);
 
 	const [selectedCard, setSelectedCard] = useState<Card>({
@@ -81,7 +100,6 @@ export default function Login() {
 		const newUser = user;
 		newUser.wallet = user?.wallet - betValue * card.odds.indexOf(odd);
 		console.log(newUser);
-		setUsers(newUser);
 		axios
 			.post('/api/create-bet', {
 				userevent_id: 1,
@@ -95,83 +113,113 @@ export default function Login() {
 			.then((res) => {});
 	};
 
-	const Logout = () => {
-		window.localStorage.removeItem('supabase.auth.token');
-		window.localStorage.removeItem('user');
-
-		Router.push('/');
-	};
-
 	return (
-		<div className={styles.container}>
-			<button onClick={Logout}>Logout...</button>
-			User: {user?.wallet}
-			<main className={styles.main}>
-				{selectedCard ? (
-					<div className={styles.selectedCard}>
-						<h2>
-							{/* {selectedCard.name} - {selectedCard.teams[selectedOdd]} */}
-							{selectedCard.name} - {selectedCard.teams}
-						</h2>
-						<p>Odd: {selectedOdd}</p>
-						<input
-							onChange={(e) => setBetValue(Number(e.target.value))}
-							type="number"
-							placeholder="Valor apostado em reais"
-						/>
-						<p>
-							Retorno esperado: R$
-							{(Number(selectedOdd) * betValue).toFixed(2)}
-						</p>
-						{betValue > 0 ? (
-							<button onClick={() => bet(selectedCard, selectedOdd)}>
-								Apostar
-							</button>
-						) : null}
-					</div>
-				) : null}
+		<div>
+			<Navbar title="Minha conta" link="/profile" />
+			<ContainerReturnStyled>
+				<ContainerStyled>
+					<ContainerTemplateStyled>
+						<CardsSection>
+							<TwoCards>
+								<Card
+									icon={<FaFutbol size={30} className="mt-3" />}
+									title="Futebol"
+									text="Ver partidas"
+								/>
+								<CardDiv>
+									<Card
+										icon={<MdSportsHandball size={30} className="mt-3" />}
+										title="Handebol"
+										text="Ver partidas"
+									/>
+								</CardDiv>
+							</TwoCards>
 
-				<div className={styles.grid}>
-					{events.map((event) => (
-						<div
-							key={event.event_id}
-							className={styles.event}
-							onClick={() =>
-								setSelectedCard({ name: event.name, teams: event.teams })
-							}
-						>
-							<h2>{event.name}</h2>
-							<p>{`${event.teams[0]} X ${event.teams[1]}`}</p>
-							<button
-								onClick={() => {
-									setSelectedCard(event);
-									setSelectedOdd(event.odds[0]);
-								}}
-							>
-								{event.odds[0]}
-							</button>{' '}
-							X{' '}
-							<button
-								onClick={() => {
-									setSelectedCard(event);
-									setSelectedOdd(event.odds[1]);
-								}}
-							>
-								{event.odds[1]}
-							</button>{' '}
-							X{' '}
-							<button
-								onClick={() => {
-									setSelectedCard(event);
-									setSelectedOdd(event.odds[2]);
-								}}
-							>
-								{event.odds[2]}
-							</button>
+							<TwoCardsSecond>
+								<Card
+									icon={<GiPartyPopper size={30} className="mt-3" />}
+									title="Festas"
+									text="Ver festas"
+								/>
+								<CardDiv>
+									<Card
+										icon={<BsCalendarEvent size={30} className="mt-3" />}
+										title="Eventos"
+										text="Ver eventos"
+									/>
+								</CardDiv>
+							</TwoCardsSecond>
+						</CardsSection>
+					</ContainerTemplateStyled>
+				</ContainerStyled>
+
+				<main className={styles.main}>
+					{selectedCard ? (
+						<div className={styles.selectedCard}>
+							<h2>
+								{/* {selectedCard.name} - {selectedCard.teams[selectedOdd]} */}
+								{selectedCard.name} - {selectedCard.teams}
+							</h2>
+							<p>Odd: {selectedOdd}</p>
+							<input
+								onChange={(e) => setBetValue(Number(e.target.value))}
+								type="number"
+								placeholder="Valor apostado em reais"
+							/>
+							<p>
+								Retorno esperado: R$
+								{(Number(selectedOdd) * betValue).toFixed(2)}
+							</p>
+							{betValue > 0 ? (
+								<button onClick={() => bet(selectedCard, selectedOdd)}>
+									Apostar
+								</button>
+							) : null}
 						</div>
-					))}
-				</div>
-			</main>
+					) : null}
+
+					<div className={styles.grid}>
+						{events.map((event) => (
+							<div
+								key={event.event_id}
+								className={styles.event}
+								onClick={() =>
+									setSelectedCard({ name: event.name, teams: event.teams })
+								}
+							>
+								<h2>{event.name}</h2>
+								<p>{`${event.teams[0]} X ${event.teams[1]}`}</p>
+								<button
+									onClick={() => {
+										setSelectedCard(event);
+										setSelectedOdd(event.odds[0]);
+									}}
+								>
+									{event.odds[0]}
+								</button>{' '}
+								X{' '}
+								<button
+									onClick={() => {
+										setSelectedCard(event);
+										setSelectedOdd(event.odds[1]);
+									}}
+								>
+									{event.odds[1]}
+								</button>{' '}
+								X{' '}
+								<button
+									onClick={() => {
+										setSelectedCard(event);
+										setSelectedOdd(event.odds[2]);
+									}}
+								>
+									{event.odds[2]}
+								</button>
+							</div>
+						))}
+					</div>
+				</main>
+			</ContainerReturnStyled>
 		</div>
 	);
 }
