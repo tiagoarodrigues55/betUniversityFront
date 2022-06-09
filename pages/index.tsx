68dff593c5f6f98f415efb7a5074a8eb5d7ad373
Edmar Alencar <edmaaralencar1@gmail.com>
2022-06-09 16:17:10 -0300

    feat: create home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,72 @@
 import { useState, useEffect } from 'react';
 
-import { useAuth } from '../hooks/auth/auth';
 import axios from 'axios';
-import LoadingScreen from '../components/LoadingScreen';
-import FooterBar from '../components/FooterBar'
-import { Navbar } from '../components/Navbar'
-import Feed from './feed'
-import Login from './Login'
-import UserPage from './userPage'
 
-export default function Index() {
-	const { setData, user } = useAuth();
-	const [page, setPage] = useState("feed")
-	const [loading, setLoading] = useState(false);
-	const pages = {
-		feed: <Feed />,
-		userPage: <UserPage />
+import ModalBet from '../components/ModalBet';
+import Games from '../components/Games';
+import Sports from '../components/Sports';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+
+export default function Home() {
+	const [games, setGames] = useState([]);
+	const [sport, setSport] = useState('futebol');
+	const [modalBet, setModalBet] = useState<any>({
+		isModalOpen: false,
+		game: null,
+	});
+
+	function handleCloseModal() {
+		setModalBet({ isModalOpen: false, game: null });
 	}
-	return (
 
-		// user ? (
-		// 	<Login />
-		// ) : (
+	function handleOpenModal({ game }) {
+		setModalBet({ isModalOpen: true, game });
+	}
+
+	function handleChooseSport(sport: string) {
+		setSport(sport);
+		axios.get('/api/get-event').then((res) => {
+			setGames(
+				res.data.events.filter((game) => game.modality.toLowerCase() === sport)
+			);
+		});
+	}
+
+	useEffect(() => {
+		axios.get('/api/get-event').then((res) => {
+			setGames(
+				res.data.events.filter((game) => game.modality.toLowerCase() === sport)
+			);
+		});
+	}, []);
+
+	return (
 		<>
-			<Navbar title="Interbet" link="http://localhost:3000" />
-			{pages[page]}
-			{/* <FooterBar/> */}
+			<ModalBet
+				handleCloseModal={handleCloseModal}
+				isModalOpen={modalBet.isModalOpen}
+				game={modalBet.game}
+			/>
+			<Sports sport={sport} handleChooseSport={handleChooseSport} />
+			<Games games={games} handleOpenModal={handleOpenModal} />
 		</>
-		// )
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/login',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+};
