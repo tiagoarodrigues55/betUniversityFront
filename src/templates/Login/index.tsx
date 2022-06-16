b53874f8fde6c6c7f5121b3b495a7261137f0c1e
tiagoamericano <v-tiago.americano@stilingue.com.br>
2022-06-16 13:46:49 -0300

    fix: login

--- src/templates/Login/index.tsx
+++ src/templates/Login/index.tsx
@@ -3,7 +3,7 @@ import Logo from '../../components/Logo';
 import * as S from './styles';
 
 function LoginTemplate() {
-	if (window !== undefined) {
+	if (typeof window !== "undefined") {
 		return (
 			<S.Wrapper>
 				<div>
